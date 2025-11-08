d98ecb83e76d4de34d13e25a44092afefac1305c
tylerkilburn <tylerkilburn@gmail.com>
2025-11-08 14:27:05 -0500

    feat: migrates GenerateRandomString to utils module

--- utils/utils.go
+++ utils/utils.go
@@ -5,13 +5,13 @@ import (
 	"time"
 )
 
-func GenerateRandomString(length int) string {
-	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456788"
+const CHARACTER_SET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456788"
 
+func GenerateRandomString(length int) string {
 	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
 	b := make([]byte, length)
 	for i := range b {
-		b[i] = charset[seededRand.Intn(len(charset))]
+		b[i] = CHARACTER_SET[seededRand.Intn(len(CHARACTER_SET))]
 	}
 	return string(b)
 }
